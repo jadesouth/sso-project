8b20284410ab6915a39bbc5add0a367cf373daa5
wangnanphp <wangnanphp@163.com>
2017-02-07 17:24:28 +0830

    Token类的一些接口定义

---
+++ user_center/Token.class.php
@@ -0,0 +1,31 @@
+<?php
+
+class Token()
+{
+	public static function generateToken()
+    {
+    }
+
+	public static function checkToken()
+    {
+	
+	}
+
+	public static function getToken()
+	{
+	
+	}
+
+	public static function saveToken()
+	{
+	
+	}
+
+	public static function deleteToken()
+	{
+	}
+
+	public static function cleanToken()
+	{
+	}
+}
