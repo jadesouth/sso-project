2c7c70aacc5f5ea4a02aee0047c978ae3c34eb85
wangnan <wangnanphp@163.com>
2017-02-14 17:41:58 +0830

    sso服务端登录功能完善

--- sso/login.php
+++ sso/login.php
@@ -3,26 +3,35 @@
  * 服务端用户登录逻辑处理脚本
  */
 
+use sso\server\SSOServer;
+
 // 引入需要的文件
-include(__DIR__ . DIRECTORY_SEPARATOR . 'sso_server' . DIRECTORY_SEPARATOR . 'SSOServer.class.php');
-include(__DIR__ . DIRECTORY_SEPARATOR . 'sso_server' . DIRECTORY_SEPARATOR . 'User.class.php');
-include(__DIR__ . DIRECTORY_SEPARATOR . 'sso_server' . DIRECTORY_SEPARATOR . 'Token.class.php');
+include(__DIR__ . DIRECTORY_SEPARATOR . 'server' . DIRECTORY_SEPARATOR . 'SSOServer.class.php');
+include(__DIR__ . DIRECTORY_SEPARATOR . 'server' . DIRECTORY_SEPARATOR . 'User.class.php');
+include(__DIR__ . DIRECTORY_SEPARATOR . 'server' . DIRECTORY_SEPARATOR . 'Token.class.php');
 
 // 检验登录
 if ('POST' == $_SERVER['REQUEST_METHOD']) {
-    $user_name = $_POST['user_name'];
-    $password  = $_POST['password'];
-    $result = SSOService::login($user_name, $password);
+    $username = trim($_POST['username']);
+    $password = $_POST['password'];
+    $result = SSOServer::login($username, $password);
 
-    if (0 == $result) {
-        die('SUCCESS');
+    if (0 == $result) { // 登录成功
+        session_start();
+        $return = [
+            'is_login' => true,
+            'msg'      => 'SUCCESS',
+            'token'    => $_SESSION['token']['token'],
+            'user'     => ['id' => $_SESSION['user']['id'],'username' => $_SESSION['user']['username']],
+        ];
+        die(json_encode($return, true));
     } elseif (-1 == $result) {
-        die('USER NOT EXISTS.');
+        die(json_encode(['is_login' => false, 'msg' => 'USER NOT EXISTS.']));
     } elseif (-2 == $result) {
-        die('PASSWORD ERROR.');
+        die(json_encode(['is_login' => false, 'msg' => 'PASSWORD ERROR.']));
     } else {
-        die('UNKNOWN ERROR.');
+        die(json_encode(['is_login' => false, 'msg' => 'UNKNOWN ERROR.']));
     }
 } else {
-    die('OPERATION NOT ALLOWED.');
+    die(json_encode(['is_login' => false, 'msg' => 'OPERATION NOT ALLOWED.'], true));
 }
