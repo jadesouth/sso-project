24ffb41d2205955b8175df3a5be5370869c506a6
wangnan <wangnanphp@163.com>
2017-02-15 17:21:00 +0830

    CURL增加设置cookie方法

--- sso/lib/CURL.class.php
+++ sso/lib/CURL.class.php
@@ -12,7 +12,7 @@ class CURL
      */
     private $curl = null;
     /**
-     * @var CURL设置选项
+     * @var array CURL设置选项
      */
     private $opt = [
         CURLOPT_RETURNTRANSFER => true,
@@ -31,6 +31,10 @@ class CURL
 
     /**
      * post POST请求数据
+     *
+     * @param array $post_data POST请求的数据
+     *
+     * @return string curl请求的数据
      */
     public function post(array $post_data): string
     {
@@ -42,11 +46,30 @@ class CURL
 
     /**
      * get GET请求数据
+     *
+     * @return string curl请求返回的数据
      */
-    public function get(array $get_data): string
+    public function get(): string
     {
         curl_setopt_array($this->curl, $this->opt);
         return curl_exec($this->curl);
+    }
+
+    /**
+     * setCookies 设置请求的Cookies
+     *
+     * @param array $cookies 键为 cookie name 值为 cookie value 的数组
+     *
+     * @return void
+     */
+    public function setCookies(array $cookies): void
+    {
+        foreach ($cookies as $cookie_name => $cookie_value) {
+            $format_cookies[] = "{$cookie_name}={$cookie_value}";
+        }
+        $format_cookies = implode($format_cookies, '; ');
+
+        curl_setopt($this->curl, CURLOPT_COOKIE, $format_cookies);
     }
 
     /**
@@ -63,13 +86,5 @@ class CURL
     public function setOpt(array $opt)
     {
         $this->opt = $opt;
-    }
-
-    /**
-     * CURL destruct.
-     */
-    public function __destruct()
-    {
-        empty($this->curl) || curl_exec($this->curl);
     }
 }
