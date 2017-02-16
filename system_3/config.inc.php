4c08a76ffac251e4c988a847cb00d708932b8cfe
wangnan <wangnanphp@163.com>
2017-02-16 14:49:38 +0830

    加入系统3

---
+++ system_3/config.inc.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 项目配置文件
+ */
+
+const SSO_ACTIVE_SERVER  = 'http://www.sso.com/login.php';
+const SSO_PASSIVE_SERVER = 'http://www.sso.com/passive_login.php';
+const SESSION_PATH = '/tmp/sess/s3/';
+const LOCAL_URL = 'http://www.s3.com/';
+const SSO_OTHER_DOMAIN = [
+    'http://www.s2.com',
+    'http://www.s3.com',
+];
---
+++ system_3/index.php
@@ -0,0 +1,32 @@
+<?php
+    // 引入配置文件
+    include __DIR__ . '/config.inc.php';
+    session_save_path(SESSION_PATH);
+    session_start();
+    $is_login = empty($_SESSION['is_login']) ? false : true;
+?>
+<html>
+    <head>
+        <title>系统3</title>
+    </head>
+    <body style="margin:60px auto;text-align:center;">
+        <h2>我是系统3，域名：www.s3.com</h2>
+        <br/>
+        <?php if ($is_login):?>
+        <h3>欢迎您，<?=$_SESSION['user']['username']?></h3>
+        <a href="./logout.php">退出系统3</a>
+        <?php
+            include dirname(__DIR__) . '/sso/client/SSOClient.class.php';
+            $iFrame = sso\client\SSOClient::otherDomainLogin($_SESSION['token']);
+            echo $iFrame;
+        ?>
+        <?php else:?>
+        <h3>您还未登录,请先<a href="./login.php">登录系统3</a></h3>
+        <?php
+            include dirname(__DIR__) . '/sso/client/SSOClient.class.php';
+            $iFrame = sso\client\SSOClient::otherDomainLogout();
+            echo $iFrame;
+        ?>
+        <?php endif;?>
+    </body>
+</html>
